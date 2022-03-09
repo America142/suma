478461bc3abb24ba080303f91f711bd537f0d627
america <villalobosamericacbtis183@gmail.com>
2022-03-09 11:20:14 -0600

    este es mi primer commit

---
+++ src/clase1.java
@@ -0,0 +1,21 @@
+
+public class clase1 {
+
+	public static void main (String[] parametro) {
+		int numero1 = 0;
+		int numero2 = 0;
+		int resultado;
+		
+		System.out.println("Introduce el primer numero");
+		Object reader;
+		numero1 = (Object(reader)).nextInt();
+		
+		System.out,println("Introduce el segundo numero");
+		Object reader;
+		numero2 = ((Object)reader).nextInt();
+		
+		resultado =numero1+numero2;
+		System.out.println("la suma  es "+ numero1+ "+" + numero2 + "=" + resultado);
+	}
+	
+}
